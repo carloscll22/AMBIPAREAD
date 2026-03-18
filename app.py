ba35b10a998d02cd0b87d7a6b0975c806e4dd6b4
AMBIPAR <carloslourencosinal@gmail.com>
2026-03-18 12:29:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2475,7 +2475,8 @@ def fale_tutor():
     return redirect("/login")
 
 
-# ----------------------------- MAIN -----------------------------------
+import os
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
